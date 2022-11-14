e04132714cacb9bbb99b2d41dfbaee037d951411
Re-Li-fe <gyf1657261410@outlook.com>
2022-11-14 23:42:36 +0800

    爬虫相关

--- dataprocess/README.md
+++ dataprocess/README.md
@@ -1,3 +1,10 @@
 ## 开源供应链安全风险分析
 
 数据相关
+
+### flume
+
+```
+flume-ng agent -n a1 -c conf -f conf/example.conf
+```
+
---
+++ dataprocess/spider/checkdomain.py
@@ -0,0 +1,46 @@
+import pymysql
+import requests
+from spider import isDomainAvaliable
+
+
+def checkdomain():
+
+    # 提前维护一个字典，查过的就不再查了
+    domainDict = {}
+
+    # 打开数据库连接，参数1：主机名或IP；参数2：用户名；参数3：密码；参数4：数据库名
+    db = pymysql.connect(host='43.138.47.53', user='mycloud',
+                         password='mycloud', database='ossd')
+    cursor = db.cursor()
+    # use sql select email from human where email like '%@%';
+    sql = "select email from human where email like '%@%';"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # 遍历results
+    for i in range(len(results)):
+        domain = results[i][0].split('@')[1]
+        if domain in domainDict:
+            pass
+        else:
+            res = isDomainAvaliable(domain)
+            if 'available' in res:
+                if res['available']:
+                    domainDict[domain] = 1
+                    print(domain, '可用')
+                else:
+                    domainDict[domain] = 0
+                    print(domain, '不可用')
+            else:
+                domainDict[domain] = 1
+        print(results[i][0], end='')
+        print(domainDict[domain])
+        # 写入数据库human的expired
+        sql = "update human set expired = %d where email = '%s'" % (
+            domainDict[domain], results[i][0])
+        cursor.execute(sql)
+    db.commit()
+    db.close()
+
+
+if __name__ == '__main__':
+    checkdomain()
