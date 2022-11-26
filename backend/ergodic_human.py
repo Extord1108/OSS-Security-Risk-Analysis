a5eee51f5c707b14d8c127a3402d51d79c77af20
BXBZ <2770294376@qq.com>
2022-11-26 14:39:08 +0800

    后端整理完成

--- backend/ergodic_human.py
+++ backend/ergodic_human.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
-from model import *
+from models import *
 
 # 数据库连接url
 DB_CONNECT_STRING = 'mysql+pymysql://mycloud:mycloud@43.138.47.53/ossd'
@@ -19,24 +19,26 @@ print("-------------------------------------")
 
 Base1 = declarative_base(engine)
 session = sessionmaker(engine)()
+glob_all_human_num=0
+glob_expired_human_num=0
+glob_expired_package_num=0
 
-
-def cal_human():
-    # 计算维护者的过期比例
+def cal_human():# 计算维护者的数量
     all_human = session.query(Human).all()
     # for human in all_human:
     #     print(human.email)
-    all_num = session.query(func.count(Human.name)).first()
+    all_human_num = session.query(func.count(Human.name)).first()
     # expired_num = session.query(func.count(Human.expired == 1)).first()
     expired_human = session.query(Human).filter_by(expired=1).all()
-    expired_num=0
+    expired_human_num=0
     for i in expired_human:
-        expired_num = expired_num+1
+        expired_human_num = expired_human_num+1
     print("-------------------------------------")
-    print(str(expired_num) + "/" + str(all_num)[str(all_num).index('(') + 1:str(all_num).index(',')])
+    print({"all_human_num":all_human_num,"expired_num":expired_human_num})
+    return ({"all_human_num":all_human_num,"expired_num":expired_human_num})
 
 
-def cal_package(package_id):
+def check_package(package_id):
     package_expired = 0
     package_humans = session.query(Maintainer).filter_by(package_id=package_id).all()
     # print("-------------------------------------")
@@ -47,13 +49,18 @@ def cal_package(package_id):
 
     return package_expired
 
+def cal_package():#计算过期包的比例
+    expired_package_num = 0
+    all_package = session.query(Package).all()
+    all_package_num = session.query(func.count(Package.id)).first()
+    for package in all_package:
+        expired_package_num += check_package(package.id)
+
+    print({"all_package_num":all_package_num,"expired_package_num":expired_package_num})
+    return ({"all_package_num":all_package_num,"expired_package_num":expired_package_num})
+
 
 if __name__ == '__main__':
-    # cal_human()
+    cal_human()
     # cal_package('--ignore-workspace-root-check')
-    expired_package_num = 0
-    all_package=session.query(Package).all()
-    for package in all_package:
-        expired_package_num+=cal_package(package.id)
 
-    print(expired_package_num)
