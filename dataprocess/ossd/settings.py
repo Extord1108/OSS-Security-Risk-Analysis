384b3d5764610d95557f0ec05ebf7888655fc3b7
Re-Li-fe <gyf1657261410@outlook.com>
2022-12-11 12:50:13 +0800

    爬虫性能优化

--- dataprocess/ossd/settings.py
+++ dataprocess/ossd/settings.py
@@ -28,12 +28,12 @@ LOG_FILE = "scrapy.log"
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.1
+DOWNLOAD_DELAY = 0.2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -74,13 +74,13 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'ossd.pipelines.OssdPipeline': 300,
-   'ossd.pipelines.MysqlPipeline': 200,
+    'ossd.pipelines.OssdPipeline': 300,
+    'ossd.pipelines.MysqlPipeline': 200,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
